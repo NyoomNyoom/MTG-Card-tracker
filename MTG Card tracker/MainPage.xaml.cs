9584c0a73873b07407a96f550028eab6bbf32600
NyoomNyoom <jacksonnorth1275@gmail.com>
2024-08-29 22:19:15 +1200

    Cleaned up the main page code to make debugging easier

--- MTG Card tracker/MainPage.xaml.cs
+++ MTG Card tracker/MainPage.xaml.cs
@@ -10,15 +10,8 @@ public partial class MainPage : ContentPage
 		InitializeComponent();
 	}
 
-	private void OnCounterClicked(object sender, EventArgs e)
-	{
-		Card card = new Card("Black lotus", "2");
-		count++;
-	}
-
     private async void Tracker_Clicked(object sender, EventArgs e)
     {
-		// Navigation.PushAsync(new NavigationPage(new SecondPage()));
 		await Navigation.PushAsync(new NavigationPage(new TrackerPage()));
     }
 }
