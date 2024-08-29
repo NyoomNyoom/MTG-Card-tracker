dbd092cd8206ba7bc7a2ea1983906894f2ad0f22
NyoomNyoom <jacksonnorth1275@gmail.com>
2024-08-29 22:18:10 +1200

    Trying to get a button working to swap pages.

--- MTG Card tracker/MainPage.xaml.cs
+++ MTG Card tracker/MainPage.xaml.cs
@@ -15,5 +15,11 @@ public partial class MainPage : ContentPage
 		Card card = new Card("Black lotus", "2");
 		count++;
 	}
+
+    private async void Tracker_Clicked(object sender, EventArgs e)
+    {
+		// Navigation.PushAsync(new NavigationPage(new SecondPage()));
+		await Navigation.PushAsync(new NavigationPage(new TrackerPage()));
+    }
 }
 
---
+++ MTG Card tracker/TrackerPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace MTG_Card_tracker;
+
+public partial class TrackerPage : ContentPage
+{
+	public TrackerPage()
+	{
+		InitializeComponent();
+	}
+}
