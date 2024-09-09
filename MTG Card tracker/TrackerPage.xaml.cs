bdddb766737ee2f633fe64c8eb544a4a77a942cf
NyoomNyoom <jacksonnorth1275@gmail.com>
2024-09-09 21:30:35 +1200

    Added variables to track each players hp

--- MTG Card tracker/TrackerPage.xaml.cs
+++ MTG Card tracker/TrackerPage.xaml.cs
@@ -2,6 +2,11 @@ namespace MTG_Card_tracker;
 
 public partial class TrackerPage : ContentPage
 {
+	private int p1HP = 40;
+	private int p2HP = 40;
+	private int p3HP = 40;
+	private int p4HP = 40;
+
 	public TrackerPage()
 	{
 		InitializeComponent();
