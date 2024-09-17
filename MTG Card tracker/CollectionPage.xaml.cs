e62df41352d3ba5b736833b253be70499dd428c9
NyoomNyoom <jacksonnorth1275@gmail.com>
2024-09-17 16:29:49 +1200

    Started work on implementing the API

--- MTG Card tracker/CollectionPage.xaml.cs
+++ MTG Card tracker/CollectionPage.xaml.cs
@@ -1,9 +1,20 @@
+using MtgApiManager.Lib.Service;
+
 namespace MTG_Card_tracker;
 
 public partial class CollectionPage : ContentPage
 {
-	public CollectionPage()
+	private string searchResults = null;
+
+    IMtgServiceProvider serviceProvider = new MtgServiceProvider();
+
+    public CollectionPage()
 	{
 		InitializeComponent();
 	}
+
+    private void SearchButton_Clicked(object sender, EventArgs e)
+    {
+
+    }
 }
