769caa30bc70f51e5ca8196f0df1744d55ea8001
NyoomNyoom <jacksonnorth1275@gmail.com>
2024-09-20 01:58:15 +1200

    Really trying hard to get some resemblence of an API call to return images...

--- MTG Card tracker/CollectionPage.xaml.cs
+++ MTG Card tracker/CollectionPage.xaml.cs
@@ -7,7 +7,7 @@ namespace MTG_Card_tracker;
 public partial class CollectionPage : ContentPage
 {
 
-    IMtgServiceProvider serviceProvider = new MtgServiceProvider();
+    readonly IMtgServiceProvider serviceProvider = new MtgServiceProvider();
 
     public CollectionPage()
 	{
@@ -17,18 +17,41 @@ public partial class CollectionPage : ContentPage
     private async void SearchButton_Clicked(object sender, EventArgs e)
     {
         ICardService service = serviceProvider.GetCardService();
-        IOperationResult<List<ICard>> result = service.AllAsync().Result;
+        //IOperationResult<List<ICard>> result = service.AllAsync().Result;
+        IOperationResult<List<ICard>> result = service.Where(x => x.Name, "Archangel Avacyn").AllAsync().Result;
+ 
+        var apiResult = result.Value.ToArray();
 
-        var searchResult = result.Value.ToArray();
-
-        foreach ( var card in searchResult )
+        foreach (var card in apiResult)
         {
-            if ( card != null )
+            if (card != null)
             {
-                searchResults.Text = card.Name;
-                //searchImage.Source = ImageSource.FromUri();
+                if (card.Name != null)
+                {
+                    IOperationResult<List<ICard>> tempResult = service.Where(x => x.Name, card.Name).AllAsync().Result;
+
+                    if (tempResult.IsSuccess && tempResult.Value.First().MultiverseId != null)
+                    {
+                        searchImage.Source = ImageSource.FromUri(tempResult.Value.First().ImageUrl);
+                        searchResults.Text = card.Name;
+                    }
+                }
             }
         }
+
+
+        /**
+        var searchResult = result.Value.ToArray();
+
+            foreach ( var card in searchResult )
+            {
+                if ( card != null )
+                {
+                    searchResults.Text = card.Name;
+                    //searchImage.Source = ImageSource.FromUri();
+                }
+            }
+        **/
 
     }
 }
