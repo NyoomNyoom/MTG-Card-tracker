05db7dd61bc09b71d71bb81fb54121643f7688fb
NyoomNyoom <jacksonnorth1275@gmail.com>
2024-10-03 21:45:27 +1300

    Learning about using API's through http directly

--- MTG Card tracker/ScryfallAPIHandler.cs
+++ MTG Card tracker/ScryfallAPIHandler.cs
@@ -3,11 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Headers;
 
 namespace MTG_Card_tracker
 {
     public class ScryfallAPIHandler
     {
-           
+        public static HttpClient ScryFallClient { get; set; }
+
+        public static void InitialiseClient()
+        {
+            ScryFallClient = new HttpClient();
+            ScryFallClient.DefaultRequestHeaders.Accept.Clear();
+            ScryFallClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
     }
 }
