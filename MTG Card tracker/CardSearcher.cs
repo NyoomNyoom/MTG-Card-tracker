8425474baf0c938b159742f78acf7e0124c41887
NyoomNyoom <jacksonnorth1275@gmail.com>
2024-10-04 14:03:22 +1300

    Encoding the search term and adding it to the URL

--- MTG Card tracker/CardSearcher.cs
+++ MTG Card tracker/CardSearcher.cs
@@ -3,10 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web;
 
 namespace MTG_Card_tracker
 {
-    internal class cardSearcher
+    public class CardSearcher
     {
+        public async Task SearchScryfall(string searchTerm = null)
+        {
+            string url = null;
+
+            if (!string.IsNullOrEmpty(searchTerm))
+            {
+                string encodedSearch = HttpUtility.UrlEncode(searchTerm);
+                url = $"https://api.scryfall.com/cards/search?order=cmc&q={encodedSearch}";
+            }
+        }
     }
 }
