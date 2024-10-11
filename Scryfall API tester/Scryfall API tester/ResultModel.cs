3afca1e71df9ab6d14021371432e6703905ce21e
NyoomNyoom <jacksonnorth1275@gmail.com>
2024-10-11 19:09:42 +1300

    Code for interpreting the JSON that is returned by the API

---
+++ Scryfall API tester/Scryfall API tester/ResultModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scryfall_API_tester
+{
+    public class ResultModel
+    {
+        public String Object { get; set; }
+        public String? Next_page { get; set; }
+        public int Total_cards { get; set; }
+        public bool Has_more { get; set; }
+    }
+}
