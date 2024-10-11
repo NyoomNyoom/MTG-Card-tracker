4f02735495aa957a30bde521fb165a4ebd9cf8db
NyoomNyoom <jacksonnorth1275@gmail.com>
2024-10-11 16:43:37 +1300

    Added the API handler code

---
+++ Scryfall API tester/Scryfall API tester/APIHandler.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scryfall_API_tester
+{
+    public static class APIHandler
+    {
+        public static HttpClient? APIClient { get; set; }
+        
+        public static void InitialiseClient()
+        {
+            APIClient = new HttpClient();
+            APIClient.DefaultRequestHeaders.Accept.Clear();
+        }
+    }
+}
