b39710a392d64706ee90335ed8883c560a0612ec
NyoomNyoom <jacksonnorth1275@gmail.com>
2024-10-15 19:46:22 +1300

    Just fixing a couple of warnings of possible null value references

--- Scryfall API tester/Scryfall API tester/APIHandler.cs
+++ Scryfall API tester/Scryfall API tester/APIHandler.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,11 +10,12 @@ namespace Scryfall_API_tester
     public static class APIHandler
     {
         public static HttpClient APIClient { get; set; }
-        
+
         public static void InitialiseClient()
         {
             APIClient = new HttpClient();
             APIClient.DefaultRequestHeaders.Accept.Clear();
+            APIClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
         }
     }
 }
--- Scryfall API tester/Scryfall API tester/ResultModel.cs
+++ Scryfall API tester/Scryfall API tester/ResultModel.cs
@@ -8,7 +8,7 @@ namespace Scryfall_API_tester
 {
     public class ResultModel
     {
-        public String Object { get; set; }
+        public String? Object { get; set; }
         public String? Next_page { get; set; }
         public int Total_cards { get; set; }
         public bool Has_more { get; set; }
