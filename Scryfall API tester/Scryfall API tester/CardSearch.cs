ea89d8d73b47f2151fe3aa1a16b9e924265c762a
NyoomNyoom <jacksonnorth1275@gmail.com>
2024-10-15 19:27:49 +1300

    Added my card searching code

---
+++ Scryfall API tester/Scryfall API tester/CardSearch.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Scryfall_API_tester
+{
+    public class CardSearch
+    {
+        public async Task<ResultModel> SearchScryfall(string searchTerm = null)
+        {
+            string url;
+
+            if (!string.IsNullOrEmpty(searchTerm))
+            {
+                string encodedSearch = HttpUtility.UrlEncode(searchTerm);
+                url = $"https://api.scryfall.com/cards/search?order=cmc&q={encodedSearch}";
+                Debug.WriteLine("if: True");
+            }
+            else
+            {
+                url = HttpUtility.UrlEncode("https://api.scryfall.com/cards/search?order=cmc");
+                Debug.WriteLine("if: False");
+            }
+
+            using (HttpResponseMessage response = await APIHandler.APIClient.GetAsync(url))
+            {
+                if (response.IsSuccessStatusCode)
+                {
+                    ResultModel SearchResults = await response.Content.ReadFromJsonAsync<ResultModel>;
+
+                    return SearchResults;
+                }
+                else
+                {
+                    throw new Exception(response.ReasonPhrase);
+                }
+            }
+        }
+    }
+}
