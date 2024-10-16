00b5efcdc14ad7c0d4854e5c24655961a9df967f
NyoomNyoom <jacksonnorth1275@gmail.com>
2024-10-16 19:45:57 +1300

    Added some extra debugging steps to work out where its going wrong

--- Scryfall API tester/Scryfall API tester/CardSearch.cs
+++ Scryfall API tester/Scryfall API tester/CardSearch.cs
@@ -1,44 +1,49 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Scryfall_API_tester;
 using System.Diagnostics;
-using System.Linq;
 using System.Net.Http.Json;
-using System.Text;
-using System.Threading.Tasks;
 using System.Web;
 
-namespace Scryfall_API_tester
-{
-    public class CardSearch
-    {
-        public async Task<ResultModel> SearchScryfall(string searchTerm = null)
-        {
-            string url;
+namespace Scryfall_API_tester;
 
-            if (!string.IsNullOrEmpty(searchTerm))
+public class CardSearch
+{
+    public async Task<ResultModel> SearchScryfall(string searchTerm = null)
+    {
+        string url;
+
+        if (!string.IsNullOrEmpty(searchTerm))
+        {
+            string encodedSearch = HttpUtility.UrlEncode(searchTerm);
+            url = $"https://api.scryfall.com/cards/search?order=cmc&q={encodedSearch}";
+        }
+        else
+        {
+            url = "https://api.scryfall.com/cards/search?order=cmc";
+        }
+
+        using (HttpResponseMessage response = await APIHandler.APIClient.GetAsync(url))
+        {
+            if (response.IsSuccessStatusCode)
             {
-                string encodedSearch = HttpUtility.UrlEncode(searchTerm);
-                url = $"https://api.scryfall.com/cards/search?order=cmc&q={encodedSearch}";
-                Debug.WriteLine("if: True");
+                // Output the raw content to the console to check for errors
+                string rawContent = await response.Content.ReadAsStringAsync();
+                Debug.WriteLine("Raw Response: " + rawContent); // For debugging purposes
+
+                try
+                {
+                    // Attempt to deserialize the response to ResultModel
+                    ResultModel searchResults = await response.Content.ReadFromJsonAsync<ResultModel>();
+                    return searchResults;
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine("Deserialization Error: " + ex.Message);
+                    throw;  // Re-throw to see the error at the source level
+                }
             }
             else
             {
-                url = HttpUtility.UrlEncode("https://api.scryfall.com/cards/search?order=cmc");
-                Debug.WriteLine("if: False");
-            }
-
-            using (HttpResponseMessage response = await APIHandler.APIClient.GetAsync(url))
-            {
-                if (response.IsSuccessStatusCode)
-                {
-                    ResultModel? SearchResults = await response.Content.ReadFromJsonAsync<ResultModel>();
-
-                    return SearchResults;
-                }
-                else
-                {
-                    throw new Exception(response.Content.ToString());
-                }
+                throw new Exception($"Error: {response.StatusCode} - {response.ReasonPhrase}");
             }
         }
     }
