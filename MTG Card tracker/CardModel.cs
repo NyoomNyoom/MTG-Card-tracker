d47f76e05ce2355214bc15de7859d60e3fab625f
NyoomNyoom <jacksonnorth1275@gmail.com>
2024-10-21 19:16:33 +1300

    Getting rid of the old card model

--- MTG Card tracker/CardModel.cs
+++ MTG Card tracker/CardModel.cs
@@ -8,20 +8,6 @@ namespace MTG_Card_tracker
 {
     public class CardModel
     {
-        public String Name { get; set; }
-        public int Id { get; set; }
-        public Uri ImageURI { get; set; }
 
-        public CardModel(string NameIn, string IdIn)
-        {
-            Name = NameIn;
-            try
-            {
-                Id = Int32.Parse(IdIn);
-            } catch(FormatException)
-            {
-                Id = -1;
-            }
-        }
     }
 }
