ed49454691f1385bef37ce2cb29c6b7087cb60c9
NyoomNyoom <jacksonnorth1275@gmail.com>
2024-10-22 22:10:18 +1300

    Going back to working with the cmd line as I am still having some weird issues

--- Scryfall API tester/Scryfall API tester/Card.cs
+++ Scryfall API tester/Scryfall API tester/Card.cs
@@ -8,6 +8,35 @@ namespace Scryfall_API_tester
 {
     public class Card
     {
+        public string id { get; set; }
+        public string name { get; set; }
+        public string type_line { get; set; }
+        public string oracle_text { get; set; }
+        public string power { get; set; }
+        public string toughness { get; set; }
+        public ImageUris image_uris { get; set; }
+        public Prices prices { get; set; }
+    }
 
+    public class ImageUris
+    {
+        public string small { get; set; }
+        public string normal { get; set; }
+        public string large { get; set; }
+    }
+
+    public class Prices
+    {
+        public string usd { get; set; }
+        public string usd_foil { get; set; }
+        public string eur { get; set; }
+        public string tix { get; set; }
+    }
+
+    public class ScryfallResponse
+    {
+        public List<Card> data { get; set; }
+        public bool has_more { get; set; }
+        public string next_page { get; set; }
     }
 }
