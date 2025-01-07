9aa72d534d5f31ef87ad5d77fa291d4f9b4b9156
NyoomNyoom <jacksonnorth1275@gmail.com>
2025-01-07 17:18:49 +1300

    Create CollectionHandler.cs
    Is going to be the class that handles the users collection

---
+++ MTG Card tracker/CollectionHandler.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MTG_Card_tracker
+{
+    internal class CollectionHandler
+    {
+    }
+}
